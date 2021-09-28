aab67639692ce2fcf983711c7673b11305ade1dc
jl118 <jen.liebelt@gmail.com>
2021-09-28 14:25:16 -0400

    Added typewriter animation to hero subtitle

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,4 +21,31 @@ const navSlide = () => {
     
 }
 
+const typewriterEffect = () => {
+    const texts = ['Full Stack Web Developer', 'Graphic Designer', 'Terrible Singer', 'Mario Kart Champion'];
+    let count = 0;
+    let index = 0;
+    let currentText = '';
+    let letter = '';
+    (function type(){
+
+        if(count === texts.length) {
+            count = 0;
+        }
+        currentText = texts[count];
+        letter = currentText.slice(0, ++index);
+
+        document.querySelector('.typing').textContent = letter;
+        if(letter.length === currentText.length) {
+            count++;
+            index = 0;
+            setTimeout(type, 2000);
+        } else {
+            setTimeout(type, 200);
+        }
+        
+    }());
+}
+
+typewriterEffect();
 navSlide();
